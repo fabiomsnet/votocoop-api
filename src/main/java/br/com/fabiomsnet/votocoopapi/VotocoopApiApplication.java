d99b644e239d3e9634e940b8bde3a3e516ec50bf
Fábio <fabiomsnet@gmail.com>
2020-05-22 20:52:14 -0300

    Inicio projeto votocoop

---
+++ src/main/java/br/com/fabiomsnet/votocoopapi/VotocoopApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabiomsnet.votocoopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotocoopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotocoopApiApplication.class, args);
+	}
+
+}
